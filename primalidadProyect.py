85a69b7896e2048ebc87b99abdf175718520920a
gfreycode <gfreycode@gmail.com>
2021-05-23 17:11:41 -0500

    termine el programa para ver si un numero es primo o no es primo.

--- primalidadProyect.py
+++ primalidadProyect.py
@@ -1,5 +1,18 @@
+def is_prime(number):
+    count = 0
+    for i in range(number):
+        if number % ( i + 1 ) != 0:
+            continue
+        else:
+            count +=1
+    if count == 2:
+        print('Is prime')
+    else:
+        print('Not is prime')
+
 def run():
-    pass
+    number = int(input('Writing a number: '))
+    is_prime(number)
 
 if __name__ == '__main__':
     run()
